9b18a78183c81ccf7560a230684be2bc0fd6a31d
RakheeKaushal <130360378+RakheeKaushal@users.noreply.github.com>
2023-08-18 09:50:05 +0530

    Adding src folder

---
+++ src/main/java/HelloJava.java
@@ -0,0 +1,13 @@
+public class HelloJava {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hello World to the awesome world of Java");
+		System.out.println("This is correct file");
+		  
+		  
+	
+
+	}
+
+}
