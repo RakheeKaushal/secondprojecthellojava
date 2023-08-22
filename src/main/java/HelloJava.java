0c0ad4fda354544d7db2943c40b06e4e72b157f9
RakheeKaushal <rakhee.kaushal@kritikalvision.ai>
2023-08-22 13:53:23 +0530

    Update HelloJava.java

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -3,7 +3,6 @@ public class HelloJava {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World to the awesome world of Java");
-		System.out.println("This is correct file");
 		System.out.println("Add Login page here");
 		  
 	
